19639be38a3afe4195200fb5dfb6e0f094111b7c
Tommy Brown <thomasbrown73095@gmail.com>
2015-04-09 16:24:21 -0500

    initial commit of tweet_view

--- app/src/main/java/tommista/com/turbine2/adapters/TweetAdapter.java
+++ app/src/main/java/tommista/com/turbine2/adapters/TweetAdapter.java
@@ -1,7 +1,35 @@
 package tommista.com.turbine2.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import tommista.com.turbine2.R;
+import tommista.com.turbine2.models.Tweet;
+
 /**
  * Created by tbrown on 4/9/15.
  */
-public class TweetAdapter {
+public class TweetAdapter extends ArrayAdapter<Tweet>{
+
+    public TweetAdapter(Context context, ArrayList<Tweet> tweets) {
+        super(context, R.layout.tweet_view , tweets);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final Tweet tweet = getItem(position);
+
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.handle_view, null);
+        }
+
+        return convertView;
+    }
+
 }
