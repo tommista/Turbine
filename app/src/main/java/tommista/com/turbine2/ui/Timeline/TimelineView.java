efd0b0c88c7adbc8b2fb8d3c7473baa50fe1502c
Tommy Brown <thomasbrown73095@gmail.com>
2015-04-13 17:04:10 -0500

    wip

--- app/src/main/java/tommista/com/turbine2/ui/Timeline/TimelineView.java
+++ app/src/main/java/tommista/com/turbine2/ui/Timeline/TimelineView.java
@@ -9,30 +9,43 @@ import java.util.ArrayList;
 
 import javax.inject.Inject;
 
+import tommista.com.turbine2.R;
 import tommista.com.turbine2.TurbineApp;
+import tommista.com.turbine2.adapters.TweetAdapter;
 import tommista.com.turbine2.models.Handle;
+import tommista.com.turbine2.models.Tweet;
 import tommista.com.turbine2.net.TwitterAPI;
 
 import static tommista.com.turbine2.TurbineModule.HandleList;
+import static tommista.com.turbine2.TurbineModule.TweetList;
 
 public class TimelineView extends LinearLayout{
 
-    public ListView listView;
+    private Context context;
+    private ListView listView;
+    private TweetAdapter tweetAdapter;
 
-    @Inject
-    TwitterAPI twitterAPI;
+    @Inject TwitterAPI twitterAPI;
 
-    @Inject @HandleList
-    ArrayList<Handle> handleList;
+    @Inject @HandleList ArrayList<Handle> handleList;
+    @Inject @TweetList ArrayList<Tweet> tweetList;
 
     public TimelineView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
         TurbineApp.get(context).inject(this);
     }
 
     @Override
     protected void onFinishInflate(){
         super.onFinishInflate();
+
+        listView = (ListView) findViewById(R.id.main_list_view);
+
+        tweetAdapter = new TweetAdapter(context, tweetList);
+
+        listView.setAdapter(tweetAdapter);
+
     }
 
 }
