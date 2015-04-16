247f507b61e1867bb2ce048fa57b11eb5c55811a
Tommy Brown <thomasbrown73095@gmail.com>
2015-04-16 10:06:01 -0500

    Finished writing UnshortenAPI

--- app/src/main/java/tommista/com/turbine2/net/UnshortenAPI.java
+++ app/src/main/java/tommista/com/turbine2/net/UnshortenAPI.java
@@ -1,7 +1,21 @@
 package tommista.com.turbine2.net;
 
-/**
- * Created by tbrown on 4/16/15.
- */
+import retrofit.RestAdapter;
+import tommista.com.turbine2.net.services.UnshortenService;
+
 public class UnshortenAPI {
+
+    private static final String SERVER_ADDRESS = "http://api.unshorten.it";
+    private final RestAdapter restAdapter;
+
+    public UnshortenService extensionService;
+
+    public UnshortenAPI() {
+        restAdapter = new RestAdapter.Builder()
+                .setEndpoint(SERVER_ADDRESS)
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .build();
+
+        extensionService = restAdapter.create(UnshortenService.class);
+    }
 }
